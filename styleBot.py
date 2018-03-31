cbde6960ad83e9e5c113606b2cafc71c48780314
Clarence W Cromwell <clarencewcromwell@gmail.com>
2018-03-31 11:54:05 -0700

    revised script to count paragraph and line numbers

--- styleBot.py
+++ styleBot.py
@@ -1,30 +1,36 @@
-#!/usr/bin/python3
-#This script finds style errors and sends feedback to the command line
+#!/Library/Frameworks/Python.framework/Versions/3.6/bin/python3
 
+
+
+
+#Get filename from command line input
 import sys
-
-
 myFile = sys.argv[1]
+print("styleBot is checking ", myFile)
 
+# open file and initiate line counter
 data =  open(myFile, 'r', encoding = None, errors = 'ignore')
-myPage = "<!doctype html> "
-#myPage = []
+paragraphCount = 0
 lineCount = 0
 
-#cut lines containing unwanted text string
+# Check each line against checklist of potential errors
 for line in data:
-     lineCount = lineCount + 1
-     if isinstance(line, str) == True:
-        if 'aint' in line:
-            print("line ", lineCount, ": Warning: Aint is an improper usage. Try 'am not' or 'are not' or something else.")
-        if ' -- ' in line:
-            print("line ", lineCount, ": Warning: Two consecutive hyphens. Try replacing with an em-dash. You can use alt+0151 on the PC or Command + hyphen on the Mac.")
+    myLines = line.split(".")
+    paragraphCount = paragraphCount + 1
+    lineCount = 0
+    for sentence in myLines:
+        lineCount = lineCount + 1
+        if isinstance(line, str) == True:
+           if 'aint' in sentence:
+              print("Par",paragraphCount,", Sen",lineCount, ": Warning! Aint is an improper usage. Try 'am not' or 'are not' or something else.")
+           if '--' in sentence:
+              print("Par",paragraphCount,", Sen",lineCount, ": Warning! Two consecutive hyphens. Try replacing with an em-dash. You can use alt+0151 on the PC or Command + hyphen on the Mac.")
+           if ' -- ' in sentence:
+              print("Par",paragraphCount,", Sen",lineCount, ": Warning! Two consecutive hyphens. Try replacing with an em-dash. You can use alt+0151 on the PC or Command + hyphen on the Mac.")
+
+           else:
+              continue
         else:
-           continue
-     else:
-        print("not string")
-
-
-
-
+           print("not string data")
+# Indicate the end of the process in the command line
 print("Review completed.")
